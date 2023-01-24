e3c3f4229da7fcdb92ba12e5c982e04b56f2395f
fatmatnani <you@example.com>
2023-01-24 01:42:10 -0800

    readme file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * struct flags - struct containing flags 
+ * to put on "on"
+ * @plus: flag for the '+' character
+ * @space: flah for the ' ' character
+ * @hash: flag for the '#' character
+ */
+
